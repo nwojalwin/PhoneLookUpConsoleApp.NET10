37ae97f435d4fe52568952fefd2f961065fd0bc6
nwojalwin <148061560+nwojalwin@users.noreply.github.com>
2026-02-15 15:14:43 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿#region Contact
+#region Contact
 public class Contact
 {
     public Contact(string name, string number)
@@ -78,23 +78,71 @@ public class PhoneBook
     {
         public static void Main()
         {
-            PhoneBook myPhoneBook = new PhoneBook("Natalia", "606 909 606");
-            Console.WriteLine($"Phone owner: {myPhoneBook.Owner}");
-            Console.WriteLine($"Phone owner's number: {myPhoneBook.OwnerNumber}");
+        //v1
+        #region
+        //PhoneBook myPhoneBook = new PhoneBook("Natalia", "606 909 606");
+        //Console.WriteLine($"Phone owner: {myPhoneBook.Owner}");
+        //Console.WriteLine($"Phone owner's number: {myPhoneBook.OwnerNumber}");
 
-            // Dodawanie kontaktów
-            myPhoneBook.AddContact(new Contact("Marta", "555 666 777"));
-            myPhoneBook.AddContact(new Contact("Ania", "666 888 999"));
-            myPhoneBook.AddContact(new Contact("Bartek", "555 555 555"));
+        //// Dodawanie kontaktów
+        //myPhoneBook.AddContact(new Contact("Marta", "555 666 777"));
+        //myPhoneBook.AddContact(new Contact("Ania", "666 888 999"));
+        //myPhoneBook.AddContact(new Contact("Bartek", "555 555 555"));
 
-            // Wyświetlanie kontaktów
-            myPhoneBook.ShowAllContacts();
+        //// Wyświetlanie kontaktów
+        //myPhoneBook.ShowAllContacts();
 
-            // Znalezienie numeru dla kontaktu
-            myPhoneBook.FindNumberFor("Ania");
+        //// Znalezienie numeru dla kontaktu
+        //myPhoneBook.FindNumberFor("Ania");
 
-            // Sprawdzenie, kto dzwoni
-            myPhoneBook.WhoIsCalling("555 215 555");
+        //// Sprawdzenie, kto dzwoni
+        //myPhoneBook.WhoIsCalling("555 215 555");
+        #endregion
+
+        Console.WriteLine("Phone LookUp App");
+        Console.WriteLine("Select action: ");
+        Console.WriteLine("1 - Add contact");
+        Console.WriteLine("2 - Display all contacts");
+        Console.WriteLine("3 - Display contact by number");
+        Console.WriteLine("4 - Search contact number");
+        Console.WriteLine("x - Edit");
+        var userInput = Console.ReadLine();
+        var phoneBook = new PhoneBook("User", "");
+
+        while(true)
+        {
+            switch (userInput)
+            {
+                case "1":
+                    Console.WriteLine("Insert number:");
+                    var number = Console.ReadLine();
+                    Console.WriteLine("Insert name:");
+                    var name = Console.ReadLine();
+                    Contact contact = new Contact(name, number);
+                    phoneBook.AddContact(contact);
+                    break;
+                case "2":
+                    phoneBook.ShowAllContacts();
+                    break;
+                case "3":
+                    Console.WriteLine("Write searching number:");
+                    var searchingNumber = Console.ReadLine();
+                    phoneBook.WhoIsCalling(searchingNumber);
+                    break;
+                case "4":
+                    Console.WriteLine("Write contact's name:");
+                    var searchingName = Console.ReadLine();
+                    phoneBook.FindNumberFor(searchingName);
+                    break;
+                case "x":
+                    return;
+                default:
+                    Console.WriteLine("Invalid operation");
+                    break;
+            }
+            Console.WriteLine("Select action:");
+            userInput = Console.ReadLine();
         }
     }
+}
 #endregion
